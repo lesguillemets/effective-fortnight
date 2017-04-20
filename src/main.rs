4014c985f274fd3df97709481a59f3c111b02587
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-20 17:44:02 +0900

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate image;
+
+use std::fs::File;
+use std::io::Read;
+use std::io::Bytes;
+use image::*;
+
+mod encode;
+
+// use encode;
+
+fn main() {
+    println!("Hello, world!");
+    let mut file = match File::open("./src/main.rs"){
+        Ok(f) => f,
+        Err(r) => panic!("{}",r)
+    };
+    let mut bytes = file.bytes();
+    println!("{:?}", encode::encode(&mut bytes, 100))
+}
+
