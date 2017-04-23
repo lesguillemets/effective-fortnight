4cbc6eba5c352f32351590508137342c4e2ad8a6
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-24 01:56:47 +0900

    stash

--- src/decode.rs
+++ src/decode.rs
@@ -2,7 +2,9 @@ use std::fs::File;
 use image::png;
 use image::{GenericImage, ImageDecoder};
 use image::{ColorType, Rgb, Pixel};
-pub fn from_image() {}
+pub fn from_image(f: File) -> Vec<u8> {
+    decode(read_bytes(f))
+}
 
 pub fn read_bytes(f: File) -> Vec<Vec<u8>> {
     let mut decoder = png::PNGDecoder::new(f);
@@ -16,4 +18,22 @@ pub fn read_bytes(f: File) -> Vec<Vec<u8>> {
         v.push(row);
     }
     v
+}
+
+fn decode(dat: Vec<Vec<u8>>) -> Vec<u8> {
+    let mut body = dat.into_iter().skip_while(|l| is_empty(&l));
+    let mut header = body.next().expect("empty body");
+    // FIXME chunks? but we only want the first 3
+    let size = decode_size(&[header[0], header[1], header[2]]);
+    vec![]
+}
+
+fn is_empty(line: &[u8]) -> bool {
+    let &first = line.first().expect("empty line?");
+    // want something like all :: (a -> Bool) -> [a] -> Bool
+    (first == 255 || first == 0) && line.iter().all(|&c| c == first)
+}
+
+fn decode_size(pixel: &[u8; 3]) -> usize {
+    2
 }
