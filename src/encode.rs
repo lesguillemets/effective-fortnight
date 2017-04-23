0da1f7ddf4c112ea75d518f7b17532b69088e83c
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-23 17:02:51 +0900

    Encode partially implemented, certain amount of rubbish

--- src/encode.rs
+++ src/encode.rs
@@ -2,24 +2,74 @@ use std::fs::File;
 use std::io::Read;
 use std::io::Bytes;
 use std::iter::Iterator;
+use image;
 
+// TODO
 const EMPTY_PIXEL: u8 = 255;
 
-pub fn encode(content: &mut Bytes<File>, width: usize) -> Vec<Vec<[u8; 3]>> {
-    let pixels = to_rgbs(content);
-    //                V into_iter() consumes
-    into_width(pixels.into_iter(), width)
+pub fn to_image(content: &mut Bytes<File>,
+                width: u32)
+                -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let bytes = collect_bytes(content);
+    let total_channels = bytes.len();
+    let height = {
+        let channel_width = width as usize * 3;
+        1 + ((total_channels + channel_width - 1) / channel_width)
+    };
+    // TODO : check the height, separate the chunk if necessary
+    let mut buf = image::ImageBuffer::new(width, height as u32);
+    create_first_line(&mut buf, width, total_channels);
+    let mut rgbs = to_rgbs(bytes).into_iter();
+    {
+        let mut pixels = buf.pixels_mut();
+        for _ in 0..width {
+            pixels.next();
+        }
+        while let Some(p) = rgbs.next() {
+            let pixel = pixels.next().expect("er");
+            *pixel = p;
+        }
+    }
+    buf
 }
 
-fn to_rgbs(content: &mut Bytes<File>) -> Vec<[u8; 3]> {
-    let mut pixels = Vec::new();
+fn create_first_line(buf: &mut image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+                     width: u32,
+                     len: usize) {
+    buf[(0, 0)] = encode_size(len);
+}
+
+fn encode_size(size: usize) -> image::Rgb<u8> {
+    // we want unfoldr
+    let mut v = Vec::new();
+    let mut s = size;
+    for _ in 0..3 {
+        let (d, m) = (size / 256, size % 256);
+        s = d;
+        v.push(m as u8);
+    }
+    image::Rgb([v[0], v[1], v[2]])
+}
+
+
+fn collect_bytes(content: &mut Bytes<File>) -> Vec<u8> {
+    let mut result = Vec::new();
     let mut bytes = content.map(|e| e.ok());
     while let Some(Some(r)) = bytes.next() {
+        result.push(r)
+    }
+    result
+}
+fn to_rgbs(content: Vec<u8>) -> Vec<image::Rgb<u8>> {
+    let emp = EMPTY_PIXEL; // hmm. FIXME
+    let mut pixels = Vec::new();
+    let mut bytes = content.iter();
+    while let Some(r) = bytes.next() {
         // tedious
         // FIXME; take(self, usize) -> Take(self) .. not very handy here
-        let g = joinM(bytes.next()).unwrap_or(EMPTY_PIXEL);
-        let b = joinM(bytes.next()).unwrap_or(EMPTY_PIXEL);
-        pixels.push([r, g, b]);
+        let g = bytes.next().unwrap_or(&emp);
+        let b = bytes.next().unwrap_or(&emp);
+        pixels.push(image::Rgb([*r, *g, *b]));
     }
     pixels
 }
@@ -48,3 +98,4 @@ fn into_width<T>(v: T, width: usize) -> Vec<Vec<T::Item>>
 fn joinM<T>(e: Option<Option<T>>) -> Option<T> {
     e.unwrap_or(None)
 }
+// What
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate image;
 
 use std::fs::File;
+use std::path::Path;
 use std::io::Read;
 use std::io::Bytes;
-use image::*;
+// use image;
 
 mod encode;
 
@@ -11,10 +12,12 @@ mod encode;
 
 fn main() {
     println!("Hello, world!");
-    let mut file = match File::open("./src/main.rs") {
+    let file = match File::open("./src/encode.rs") {
         Ok(f) => f,
         Err(r) => panic!("{}", r),
     };
     let mut bytes = file.bytes();
-    println!("{:?}", encode::encode(&mut bytes, 100))
+    let imgbuf = encode::to_image(&mut bytes, 30);
+    let mut outf = &mut File::create(&Path::new("out.png")).expect("fileerror");
+    image::ImageRgb8(imgbuf).save(outf, image::PNG);
 }
