442cc3e27757c663ae4a7330e53e988818924747
lesguillemets <lesguillemets@users.noreply.github.com>
2017-04-25 15:33:43 +0900

    some more tweak

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate image;
 use std::fs::File;
 use std::path::Path;
 use std::io::Read;
+use std::io::Write;
+use std::env;
 // use image;
 
 mod encode;
@@ -11,17 +13,18 @@ mod decode;
 // use encode;
 
 fn main() {
-    let file = match File::open("./src/encode.rs") {
+    let from_file = env::args().nth(1).unwrap_or("./src/decode.rs".to_owned());
+    let test_file = from_file.clone() + ".redecoded";
+    let file = match File::open(from_file) {
         Ok(f) => f,
         Err(r) => panic!("{}", r),
     };
     let mut bytes = file.bytes();
-    let imgbuf = encode::to_image(&mut bytes, 30);
+    let imgbuf = encode::to_image(&mut bytes, 800);
     let mut outf = &mut File::create(&Path::new("out.png")).expect("fileerror");
     image::ImageRgb8(imgbuf).save(outf, image::PNG);
 
     let f2 = File::open("out.png").expect("HI");
-    for c in decode::from_image(f2) {
-        print!("{}", c as char);
-    }
+    let mut test = File::create(test_file).expect("er");
+    test.write(&decode::from_image(f2));
 }
